38a9d772df3887af9aa965458f79e718313e613b
Jordi Vermeulen <j.l.vermeulen@gmail.com>
2014-12-04 21:07:47 +0100

    Fix a derp

--- Raytracer/BoundingBox.h
+++ Raytracer/BoundingBox.h
@@ -10,7 +10,7 @@ public:
 	Vector3D Center, Halfsize;
 
 	BoundingBox();
-	BoundingBox(const Vector3D& center, const Vector3D& halfsize) : Center(center), Halfsize(halfsize);
+	BoundingBox(const Vector3D& center, const Vector3D& halfsize);
 
 	static BoundingBox FromTriangles(const std::vector<Triangle>& triangles);
 
