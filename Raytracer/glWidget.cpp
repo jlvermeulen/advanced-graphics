2bae97c092e35350697cca29114cd1261ba8ffb4
Remco Kuijper <remco.kuijper@gmail.com>
2014-12-08 22:24:42 +0100

    Partial implementation of parser.

--- Raytracer/glWidget.cpp
+++ Raytracer/glWidget.cpp
@@ -1,7 +1,10 @@
 #include "glWidget.h"
 
+#include <fstream>
+
 #include <QMouseEvent>
 
+//--------------------------------------------------------------------------------
 GLWidget::GLWidget(QWidget* parent)
   : QGLWidget(parent),
     xRot(0),
@@ -11,11 +14,260 @@ GLWidget::GLWidget(QWidget* parent)
 
 }
 
+//--------------------------------------------------------------------------------
 GLWidget::~GLWidget()
 {
 
 }
 
+//--------------------------------------------------------------------------------
+void GLWidget::loadScene(QString& fileName)
+{
+  // Open file stream
+  std::ifstream fin;
+  fin.open(fileName.toUtf8().data());
+
+  std::string line;
+
+  while (std::getline(fin, line))
+    parseLine(line);
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::parseLine(std::string& line)
+{
+  IIterator begin = line.begin();
+  IIterator iterator = line.begin();
+  IIterator end = line.end();
+
+  // Read until first white-space or end-of-file
+  while (iterator != line.end() && *iterator != ' ')
+    ++iterator;
+
+  // Corner-case where iterator == begin is not handled!
+  std::string type(begin, iterator - 1);
+
+  switch (parseType(type))
+  {
+    case objType::face:
+      parseFace(++iterator, end);
+      break;
+
+    case objType::library:
+      parseLibrary(++iterator, end);
+      break;
+
+    case objType::material:
+      parseMaterial(++iterator, end);
+      break;
+
+    case objType::normal:
+      parseNormal(++iterator, end);
+      break;
+
+    case objType::space:
+      parseSpace(++iterator, end);
+      break;
+
+    case objType::texCoords:
+      parseTexCoords(++iterator, end);
+      break;
+
+    case objType::vertex:
+      parseVertex(++iterator, end);
+      break;
+
+    case objType::nil:
+    default:
+      // Don't do anything
+      break;
+  }
+
+  //size_t length = line.length();
+
+  //std::vector<std::string> args;
+  //std::string arg;
+
+  //size_t startPos = 0;
+  //size_t endPos;
+
+  //do 
+  //{
+  //  // Find end of argument
+  //  endPos = line.find(' ', startPos);
+
+  //  // Retrieve argument
+  //  if (endPos == std::string::npos)
+  //    arg = line.substr(startPos);
+  //  else
+  //    arg = line.substr(startPos, endPos - startPos);
+
+  //  // Add argument
+  //  args.push_back(arg);
+
+  //  // Move to next character
+  //  if (endPos < length)
+  //    startPos = endPos + 1;
+  //  else
+  //    startPos = std::string::npos;
+  //} while (startPos != std::string::npos);
+}
+
+//--------------------------------------------------------------------------------
+objType GLWidget::parseType(std::string& type)
+{
+  if (type == "v")
+    return objType::vertex;
+  else if (type == "vt")
+    return objType::texCoords;
+  else if (type == "vn")
+    return objType::normal;
+  else if (type == "vp")
+    return objType::space;
+  else if (type == "f")
+    return objType::face;
+  else if (type == "mtllib")
+    return objType::library;
+  else if (type == "usemtl")
+    return objType::material;
+  else
+    return objType::nil;
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::parseFace(IIterator& iterator, const IIterator& end)
+{
+  std::vector<int> vertices;
+  std::vector<int> texCoords;
+  std::vector<int> normals;
+
+  while (false) // TODO: Read only until next letter
+  {
+    // TODO: make parts optional?
+    vertices.push_back(parseInteger(iterator, end));
+    texCoords.push_back(parseInteger(iterator, end));
+    normals.push_back(parseInteger(iterator, end));
+  }
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::parseLibrary(IIterator& iterator, const IIterator& end)
+{
+  // TODO: mtllib import
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::parseMaterial(IIterator& iterator, const IIterator& end)
+{
+  // TODO: usemtl
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::parseNormal(IIterator& iterator, const IIterator& end)
+{
+  double x = parseDouble(iterator, end);
+  double y = parseDouble(iterator, end);
+  double z = parseDouble(iterator, end);
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::parseSpace(IIterator& iterator, const IIterator& end)
+{ // Do we want to support this?
+  double u = parseDouble(iterator, end);
+  double v = parseDouble(iterator, end);
+  // double w = parseDouble(iterator, end);
+  double w = 1.0;
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::parseTexCoords(IIterator& iterator, const IIterator& end)
+{
+  double u = parseDouble(iterator, end);
+  double v = parseDouble(iterator, end);
+  // double w = parseDouble(iterator, end);
+  double w = 1.0;
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::parseVertex(IIterator& iterator, const IIterator& end)
+{
+  double x = parseDouble(iterator, end);
+  double y = parseDouble(iterator, end);
+  double z = parseDouble(iterator, end);
+  // double w = parseDouble(iterator, end);
+  double w = 1.0;
+}
+
+//--------------------------------------------------------------------------------
+double GLWidget::parseDouble(IIterator& iterator, const IIterator& end)
+{
+  IIterator begin(iterator);
+
+  // Read until first white-space or end-of-file
+  while (iterator != end && *iterator != ' ')
+    ++iterator;
+
+  // Corner-case, where equal
+  std::string value(begin, iterator - 1);
+
+  return atof(value.c_str());
+}
+
+//--------------------------------------------------------------------------------
+int GLWidget::parseInteger(IIterator& iterator, const IIterator& end)
+{
+  IIterator begin(iterator);
+
+  // Read until first white-space or end-of-file
+  while (iterator != end && *iterator != ' ' && *iterator != '/')
+    ++iterator;
+
+  // Corner-case, where equal
+  std::string value(begin, iterator - 1);
+
+  return atoi(value.c_str());
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::setXRotation(int angle)
+{
+  qNormalizeAngle(angle);
+
+  if (angle != xRot)
+  {
+    xRot = angle;
+    emit xRotationChanged(angle);
+    updateGL();
+  }
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::setYRotation(int angle)
+{
+  qNormalizeAngle(angle);
+
+  if (angle != yRot)
+  {
+    yRot = angle;
+    emit xRotationChanged(angle);
+    updateGL();
+  }
+}
+
+//--------------------------------------------------------------------------------
+void GLWidget::setZRotation(int angle)
+{
+  qNormalizeAngle(angle);
+
+  if (angle != zRot)
+  {
+    zRot = angle;
+    emit xRotationChanged(angle);
+    updateGL();
+  }
+}
+
+//--------------------------------------------------------------------------------
 void GLWidget::initializeGL()
 {
   qglClearColor(QColor::fromRgb(0, 0, 0, 255));
@@ -33,6 +285,7 @@ void GLWidget::initializeGL()
   glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
 }
 
+//--------------------------------------------------------------------------------
 void GLWidget::resizeGL(const int& w, const int& h)
 {
   glViewport(0, 0, (GLint) w, (GLint) h);
@@ -43,6 +296,7 @@ void GLWidget::resizeGL(const int& w, const int& h)
   glMatrixMode(GL_MODELVIEW);
 }
 
+//--------------------------------------------------------------------------------
 void GLWidget::paintGL()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -62,11 +316,13 @@ void GLWidget::paintGL()
   glEnd();
 }
 
+//--------------------------------------------------------------------------------
 void GLWidget::mousePressEvent(QMouseEvent* event)
 {
   lastPos = event->pos();
 }
 
+//--------------------------------------------------------------------------------
 void GLWidget::mouseMoveEvent(QMouseEvent* event)
 {
   int dx = event->x() - lastPos.x();
@@ -84,51 +340,11 @@ void GLWidget::mouseMoveEvent(QMouseEvent* event)
   }
 }
 
-static void qNormalizeAngle(int &angle)
+//--------------------------------------------------------------------------------
+void GLWidget::qNormalizeAngle(int& angle)
 {
     while (angle < 0)
         angle += 360 * 16;
     while (angle > 360 * 16)
         angle -= 360 * 16;
-}
-
-void GLWidget::loadScene(QString& fileName)
-{
-  // Parse the file
-}
-
-void GLWidget::setXRotation(int angle)
-{
-  qNormalizeAngle(angle);
-
-  if (angle != xRot)
-  {
-    xRot = angle;
-    emit xRotationChanged(angle);
-    updateGL();
-  }
-}
-
-void GLWidget::setYRotation(int angle)
-{
-  qNormalizeAngle(angle);
-
-  if (angle != yRot)
-  {
-    yRot = angle;
-    emit xRotationChanged(angle);
-    updateGL();
-  }
-}
-
-void GLWidget::setZRotation(int angle)
-{
-  qNormalizeAngle(angle);
-
-  if (angle != zRot)
-  {
-    zRot = angle;
-    emit xRotationChanged(angle);
-    updateGL();
-  }
 }
--- Raytracer/glWidget.h
+++ Raytracer/glWidget.h
@@ -2,6 +2,20 @@
 
 #include <QGLWidget>
 
+typedef std::_String_iterator<std::_String_val<std::_Simple_types<char>>> IIterator;
+
+enum objType
+{
+  face,
+  normal,
+  space,
+  texCoords,
+  vertex,
+  library,
+  material,
+  nil
+};
+
 class GLWidget : public QGLWidget
 {
   Q_OBJECT
@@ -10,8 +24,34 @@ public:
   GLWidget(QWidget* parent);
   ~GLWidget();
 
-public slots:
+public:
   void loadScene(QString& fileName);
+
+protected:
+  void parseLine(std::string& line);
+  objType parseType(std::string& type);
+
+  void parseFace(IIterator& iterator, const IIterator& end);
+  void parseLibrary(IIterator& iterator, const IIterator& end);
+  void parseMaterial(IIterator& iterator, const IIterator& end);
+  void parseNormal(IIterator& iterator, const IIterator& end);
+  void parseSpace(IIterator& iterator, const IIterator& end);
+  void parseTexCoords(IIterator& iterator, const IIterator& end);
+  void parseVertex(IIterator& iterator, const IIterator& end);
+
+  double parseDouble(IIterator& iterator, const IIterator& end);
+  int parseInteger(IIterator& iterator, const IIterator& end);
+
+  void initializeGL();
+  void resizeGL(const int& w, const int& h);
+  void paintGL();
+
+  void mousePressEvent(QMouseEvent* event);
+  void mouseMoveEvent(QMouseEvent* event);
+
+  static void qNormalizeAngle(int& angle);
+
+public slots:
   void setXRotation(int angle);
   void setYRotation(int angle);
   void setZRotation(int angle);
@@ -20,14 +60,6 @@ signals:
   void xRotationChanged(int angle);
   void yRotationChanged(int angle);
   void zRotationChanged(int angle);
-
-protected:
-  void initializeGL();
-  void resizeGL(const int& w, const int& h);
-  void paintGL();
-
-  void mousePressEvent(QMouseEvent* event);
-  void mouseMoveEvent(QMouseEvent* event);
 
 private:
   int xRot;
